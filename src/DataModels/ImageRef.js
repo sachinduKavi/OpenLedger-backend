50e02fdfddb05f90c5ca104e263f8caac905a358
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-05-06 08:29:37 +0530

    imageref

---
+++ src/DataModels/ImageRef.js
@@ -0,0 +1,13 @@
+class ImageRef {
+    #imageID
+    #xAxis
+    #uAxis
+    #scale
+    #link
+
+    constructor() {
+        console.log('Creating new Image reference instant')
+    } 
+}
+
+module.exports = ImageRef
