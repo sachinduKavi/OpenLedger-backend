9183027793ae1a1209904ef559894da570de12ea
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-05-08 19:58:51 +0530

    id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -65,7 +65,7 @@ const verificationCode = async (req, res) => {
 function createUserID(lastIdentity) {
     const prefix = lastIdentity.slice(0, 2), numID = parseInt(lastIdentity.slice(2))+1
     let newID = prefix
-    for(let i = 0; i < 8-numID.toString().length; i++) newID += "0"
+    for(let i = 0; i < 16-numID.toString().length; i++) newID += "0"
     newID += numID
     console.log(newID)
     return newID
