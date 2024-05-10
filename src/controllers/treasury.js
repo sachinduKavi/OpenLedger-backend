64e013afbcb8e0b13846903733a807cd25873419
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-05-10 17:21:51 +0530

    treasury

--- src/controllers/treasury.js
+++ src/controllers/treasury.js
@@ -1,11 +1,30 @@
 const conn = require('../SQL_Connection')
+const Treasury = require('../DataModels/Treasury') // Treasury class
+
+const {generateID}  = require('../middleware/generateID')
+
+
+// Get the last used treasury ID
+const getLastTreasuryID = async  () => {
+    const [userID] = await conn.promise().query('SELECT treasury_ID FROM treasury ORDER BY treasury_ID DESC LIMIT 1').catch(err => {
+        throw err
+    })
+    return (userID.length == 0)
+        ? generateID('TS00')
+        : generateID(userID[0]['treasury_ID'])
+}
 
 
 // Create new treasury step 01 
-const createTreasury = (req, res) => {
+const createTreasury = async (req, res) => {
     console.log('Create new treasury...')
 
-    console.log(req.body)
+    const treasury = new Treasury(
+        {
+            treasuryID: await getLastTreasuryID()
+        }
+    ) // New treasury instant
+    
 
     res.end(JSON.stringify({
         body: req.body
--- src/middleware/generateID.js
+++ src/middleware/generateID.js
@@ -1,5 +1,5 @@
 // Creating new ID by incrementing the last ID
-function createUserID(lastIdentity) {
+function generateID(lastIdentity) {
     const prefix = lastIdentity.slice(0, 2), numID = parseInt(lastIdentity.slice(2))+1
     let newID = prefix
     for(let i = 0; i < 16-numID.toString().length; i++) newID += "0"
@@ -8,6 +8,17 @@ function createUserID(lastIdentity) {
     return newID
 }
 
+
+// Get the last used treasury ID
+const getLastTreasuryID = async  () => {
+    const [userID] = await conn.promise().query('SELECT treasury_ID FROM treasury ORDER BY treasury_ID DESC LIMIT 1').catch(err => {
+        throw err
+    })
+    return (userID.length == 0)
+        ? generateID('TS00')
+        : generateID(userID[0]['treasury_ID'])
+}
+
 module.exports = {
-    createUserID
+    getLastTreasuryID
 }
