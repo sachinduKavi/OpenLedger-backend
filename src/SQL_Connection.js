f97313385934715e36689583836be3325966b520
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-06-23 04:01:24 +0530

    report

--- src/SQL_Connection.js
+++ src/SQL_Connection.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2')
 
 
 // Initializing the sql Connection for the database 
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'open_ledger'
-// })
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'open_ledger'
+})
 
 // freeDB
 // const conn = mysql.createConnection({
@@ -19,12 +19,12 @@ const mysql = require('mysql2')
 
 
 // FreeDb 
-const conn = mysql.createConnection({
-    host: 'sql.freedb.tech',
-    user: 'freedb_ledger_user',
-    password: '6n&tyApRQ2Km6JC',
-    database: 'freedb_open_ledger'
-})
+// const conn = mysql.createConnection({
+//     host: 'sql.freedb.tech',
+//     user: 'freedb_ledger_user',
+//     password: '6n&tyApRQ2Km6JC',
+//     database: 'freedb_open_ledger'
+// })
 
 // const conn = mysql.createConnection({
 //     host: '104.197.138.178',
